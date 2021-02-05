d6db0347f6fa4b08ac7a4d8b1e610b84dc9b92ca
Christian H <christian.hjelseth@gmail.com>
2021-02-05 10:12:01 +0100

    Disable autocomplete by default for ghostfield

--- QoDL.DataAnnotations.Security/GhostField/HtmlHelperExtensions.cs
+++ QoDL.DataAnnotations.Security/GhostField/HtmlHelperExtensions.cs
@@ -15,10 +15,12 @@ namespace QoDL.DataAnnotations.Security.GhostField
         /// <param name="html"></param>
         /// <param name="name">Optionally override input name and wrapper class name.
         /// If overridden the same name must be provided in <see cref="ValidateGhostFieldAttribute"/>.</param>
-        public static MvcHtmlString AddAntiSpamGhostField(this HtmlHelper html, string name = "remarks")
+        /// <param name="disableAutocomplete">Includes an autocomplete=false attribute.</param>
+        public static MvcHtmlString AddAntiSpamGhostField(this HtmlHelper html, string name = "remarks", bool disableAutocomplete = true)
         {
+            var autoCompletePart = disableAutocomplete ? " autocomplete=\"false\"" : "";
             var style = "position: fixed; transform: translateX(100vw);";
-            var content = $"<div class=\"{name}--wrapper\" style=\"{style}\"><label>Remarks</label><input class=\"remark--input\" name=\"{name}\" placeholder=\"yourname@yourdomain.com\" tabindex=\"-1\"></div>";
+            var content = $"<div class=\"{name}--wrapper\" style=\"{style}\"><label>Remarks</label><input class=\"remark--input\" name=\"{name}\" placeholder=\"yourname@yourdomain.com\" tabindex=\"-1\"{autoCompletePart}></div>";
             return new MvcHtmlString(content);
         }
     }
